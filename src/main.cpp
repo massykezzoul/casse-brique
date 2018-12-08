9f5585c79754911cc572c10134e87e670431702c
Romimap <romimap@localhost.localdomain>
2018-12-08 12:41:52 +0100

    Body fonctionne, a regler les bordures cyan

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 
 int main () {
   startProgramX();
-  Body b(0, 0, 1, 40, WRED, ' ', true, true);
-  Body b2(0, 0, 20, 1, WRED, ' ', true, true);
-  Body b3(0, 20, 1, 40, WRED, ' ', true, true);
-  Body b4(40, 0, 20, 1, WRED, ' ', true, true);
-  Body c(10, 10, 1, 1, WRED, ' ', true, false);
-  c.SetVelocity(-0.5f, -0.3f);
+  Body b(0, 0, 1, 40, WRED, ' ', true, true, false);
+  Body b2(0, 0, 20, 1, WRED, ' ', true, true, false);
+  Body b3(0, 20, 1, 40, WRED, ' ', true, true, false);
+  Body b4(40, 0, 20, 1, WRED, ' ', true, true, false);
+  Body c(10, 10, 1, 1, WBLACK, ' ', true, false, true);
+  c.SetVelocity(-0.5f, -1);
   while (1) {
     usleep(10000);
     Body::AllUpdate();
