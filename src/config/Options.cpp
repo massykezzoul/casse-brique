db6c6d07319cf6363177c0e0d9a1d07d33bb05a1
Massili Kezzoul <massy.kezzoul@gmail.com>
2018-12-19 13:42:28 +0100

    Gestion des option donnée en argument du programme, A tester prochainement

--- src/config/Options.cpp
+++ src/config/Options.cpp
@@ -1,24 +1,196 @@
-#include "Options.h"
-#include "string.h"
+#include <iostream>
+#include "option.h"
 
-using namespace std;
 
-Options::Option():type(NOTYPE),description(){};
-Options::Option(Type type,string description):type(type),description(description){};
+// La Classe Option
 
-Type Options::GetType()const{ return type; }
-string Options::GetDescription()const{ return description;}
+// Constructeurs
+	Option::Option(){}
+	
+	Option::Option(Id id,const std::string& nom,char rac,Arg arg,const std::string& description)
+		:identifiant(id),nom(nom),racourci(rac),argument(arg),description(description) {}
 
-void Options::SetType(Type optiontype){type=optiontype;}
-void Options::SetDescription(string s){description=s;}
-
-void Options::PassOption(string s){
-	if(s=='-help'){
-		SetDescription("This option gives help to the palyer");
-		SetType(HELP);
+	Option::Option(Option const &copy) {
+		identifiant = copy.get_identifiant();
+		nom = (copy.get_nom());
+		racourci = (copy.get_racourci());
+		argument = (copy.get_argument());
+		description = (copy.get_description());
 	}
+
+
+// les Getteurs
+	Option::Id Option::get_identifiant() const {
+		return identifiant;
+	}
+	std::string Option::get_nom() const {
+		return nom;
+	}
+	char Option::get_racourci() const {
+		return racourci;
+	}
+	Option::Arg Option::get_argument() const {
+		return argument;
+	}
+	std::string Option::get_description() const {
+		return description;
+	}
+
+	void Option::print() const {
+		std::cout << "Identifiant : " << identifiant << std::endl
+			<< "Nom : " << nom << std::endl
+			<< "Racourci : " << racourci << std::endl
+			<< "Type d'argumment : "<< argument << std::endl
+			<< "Description : "<< description << std::endl;
+	}
+
+// Les setteurs
+
+	void Option::set_identifiant(Option::Id id) {
+		identifiant = id;
+	}
+	void Option::set_nom(const std::string& n) {
+		nom = n;
+	}
+	void Option::set_racourci(char rac) {
+		racourci = rac;
+	}
+	void Option::set_argument(Option::Arg arg) {
+		argument = arg;
+	}
+	void Option::set_description(const std::string& des) {
+		description = des;
+	}
+
+// La surchage de l'operator
+
+	Option Option::operator=(Option const& copy) {
+		if (this != &copy) {
+			identifiant = copy.get_identifiant();
+			nom = (copy.get_nom());
+			racourci = (copy.get_racourci());
+			argument = (copy.get_argument());
+			description = (copy.get_description());
+		}
+
+		return *this;
+	}
+
+
+
+/* -------------------------------------------------------------------------- */
+// La Classe Option_tab
+// Ici se fait toute la gestion des paramètres
+// Le Constructeur
+Option_tab::Option_tab():nb(0){
+	// Définition des options disponible
+	Option opt1(Option::HELP,"help",'h',Option::NOPE,
+		"Affiche les aides du programme");
+	
+	Option opt2(Option::VERSION,"version",'v',Option::NOPE,
+		"Affiche la version du programme"); 
+	
+	Option opt3(Option::DIMENSION,"dimension",'d',Option::INT,
+		"Precisé la dimension du tableau, Doit etre entre 3 et N"); 
+	
+	Option opt4(Option::CONFIG,"config",'c',Option::STR,
+		"Donne le fichier de configuration");
+	
+	Option opt5(Option::PROBABILITY,"probability",'p',Option::FLOAT,
+		"Spécifié le pourcentage de cellule vivante dans la population");
+
+	Option opt6(Option::SPEED,"speed",'s',Option::INT,
+		"Spécifié la vitesse de défilement des generations.(en ms)");
+	/* 
+	Pour l'ajout d'une option : 
+	Option opt7(Option::Id*,"nom de l'option**",'raccourci**',Option::type_argument,
+		"courte description");
+
+		* L'identifiant de l'option (voir 'option.h' ligne:6)
+		** Doivent ếtre Unique
+	*/
+
+	// Ajout des option définit dans un tableau d'option
+	add(opt1);
+	add(opt2);
+	add(opt3);
+	add(opt4);
+	add(opt5);
+	add(opt6);
 }
 
-void Options::PrintOption(){
-	cout<<type<<" : "<<description<<endl;
+// Les methodes 
+void Option_tab::add(const Option &opt){
+	// Vérifit si l'option n'existe pas déja dans le tableau
+	if (get_option(opt.get_identifiant()) == -1) {	
+		tab[nb] = opt;
+		nb++;
+	}
+	else 
+		std::cerr << "Identifiant déjà existant dans le tableau." <<std::endl<< std::endl;
+}
+void Option_tab::print() const{
+	for (int i = 0; i < nb; ++i){
+		tab[i].print();
+		std::cout << std::endl;
+	}
+}
+Option::Id Option_tab::cherche(const std::string &nom) const{
+	for (int i = 0; i < nb; ++i) {
+		if (tab[i].get_nom() == nom)
+			return tab[i].get_identifiant();
+	}
+	return Option::NB_ID;
+}
+Option::Id Option_tab::cherche(const char rac) const{
+	for (int i = 0; i < nb; ++i) {
+		if (tab[i].get_racourci() == rac)
+			return tab[i].get_identifiant();
+	}
+	return Option::NB_ID;
+}
+
+bool Option_tab::is_nom(std::string arg) const {
+	if ((arg[0] == '-') && (arg[1] == '-') && (cherche(&arg[2]) != Option::NB_ID) )
+		return true;
+	else 
+		return false;
+}
+
+bool Option_tab::is_raccourci(std::string arg) const {
+	if ((arg[0] == '-') && (cherche(arg[1]) != Option::NB_ID) )
+		return true;
+	else 
+		return false;
+}
+
+Option::Arg Option_tab::get_argument(const std::string &nom) const{
+	for (int i = 0; i < nb; ++i) {
+		if (tab[i].get_nom() == nom)
+			return tab[i].get_argument();
+	}
+	return Option::NB_ARG;
+}
+Option::Arg Option_tab::get_argument(const Option::Id  id) const{
+	for (int i = 0; i < nb; ++i) {
+		if (tab[i].get_identifiant() == id)
+			return tab[i].get_argument();
+	}
+	return Option::NB_ARG;
+}
+int Option_tab::get_option(const Option::Id id){
+	int i = 0;
+	while (i < nb) {
+		if (tab[i].get_identifiant() == id)
+			return i;
+	++i;
+	}
+	if (i == nb)
+		return -1;
+}
+Option const &Option_tab::get_option(int index) {
+	if ((index >= 0) && (index < nb))
+		return tab[index];
+	else 
+		std::cerr << "L'option n'a pas été trouvé" << std::endl;
 }
--- src/config/Options.h
+++ src/config/Options.h
@@ -1,31 +1,116 @@
-#ifndef __OPTIONS
-#define __OPTIONS
+#ifndef OPTION_H
+#define OPTION_H
 
-#include<string.h>
-
-class Option{
+class Option {
 public:
-	enum Type{
-		HELP,
+	enum Id {
+		HELP = 0,
 		VERSION,
-		AUTHORS,
-		NOTYPE
+		DIMENSION,
+		CONFIG,
+		PROBABILITY,
+		SPEED,
+		NB_ID
 	};
+
+	enum Arg {
+		INT = 0,
+		FLOAT,
+		CHAR,
+		STR,
+		NOPE,
+		NB_ARG
+	};
+
 private:
-	Type type;
-	std::string description;
+	Id identifiant;
+	std::string nom; 		 // intitulé 
+	char racourci; 
+	Arg argument;    		 // Le type eventuelle de son argument 
+	std::string description; // Description de ce que fait l'option
+
+
 public:
 	Option();
-  	Option(Type type,std::string description);
-	//getters
-	Type GetType()const;
-	std::string GetDescription()const;
-	//setters
-	void SetType(Type optiontype);
-	void SetDescription(std::string s);
-	//methods
-	void PassOption(std::string s);
-	void PrintOptions();
+	Option(Id,const std::string&,char,Arg,const std::string&);
+
+	//Constructeur par copie
+	Option(Option const &copy);
+
+	Id get_identifiant() const;
+	std::string get_nom() const;
+	char get_racourci() const;
+	Arg get_argument() const;
+
+	std::string get_description() const;
+
+	//Affiche l'option 
+	void print() const;
+
+	void set_identifiant(Id);
+	void set_nom(const std::string&);
+	void set_racourci(char);
+	void set_argument(Arg);
+	void set_description(const std::string&);
+
+	//Surcharge de l'operateur d'affectation
+	Option operator=(Option const&);
+
 };
+
+/* ---------------------------------------------------- */
+/* La Classes Option_tab qui est un tableau d'option 	*/
+
+#define OPTION_TAB_MAX 10
+
+class Option_tab {
+private:
+	size_t nb;
+	Option tab[OPTION_TAB_MAX];
+
+public:
+	// Définitions des options prise en charge
+	Option_tab();
+
+	//Permet d'ajouter une Option au tableau
+	void add(const Option &opt);
+
+	//Afficher les options enregistrées dans le tableau
+	void print() const;
+
+	//Renvoie l'identifiant de l'option dont le nom est donné en paramètre
+	Option::Id cherche(const std::string &nom) const;
+
+	//Renvoie l'identifiant de l'option dont le racourci est donné en paramètre
+	Option::Id cherche(const char rac) const;
+
+	//Renvoie Vrai si la chaine est un nom d'une option
+	bool is_nom(std::string) const;
+
+	//Renvoie Vrai si la chaine est un raccourci d'une option
+	bool is_raccourci(std::string) const;
+
+	/*Etant donné un nom (court ou long) d’option, si un argument est requis 
+	et son type le cas échéant. */  
+	Option::Arg get_argument(const std::string &nom) const;
+
+    Option::Arg get_argument(const Option::Id) const;
+
+	// Return l'index de l'option selon son identifiant (retourne -1 si n'existe pas)
+	int get_option(const Option::Id id);
+
+	// Prend la position d'une option dans le tableau et retourne cette option
+	Option const &get_option(int index);
+
+	/*
+		La méthode qui sera Utilisé par le programme principal
+		Donnée 	: la population, argc le nombre d'argument du programme, et argv l'ensembre des arguments du programme principal
+		Resultat: Applique les modifications fournit en arguments
+	*/
+	void gere_parametre(int argc,char const *argv[]);
+
+};
+
+// Définitions des options prise en charge et retourne le tableau de ces options
 
 #endif 
