465fc62f4bb661fa8ff074c86637aafa94b6fd9d
Romimap <romimap@localhost.localdomain>
2018-12-21 08:21:50 +0100

    On peut donner un angle et une vitesse à la balle, ca sera utile quand il y aura la raquette

--- src/ball.cpp
+++ src/ball.cpp
@@ -1,22 +1,39 @@
 #include "Body.h"
 #include "ball.h"
 
+#include <math.h>
+
+#define PI 3.14159265
+
 using namespace std;
 
 /* Constructeur */
 
-Ball::Ball():posX(10.0),posY(10.0),velX(1.0),velY(0.5),body(posX, posY, velX, velY, WCYAN ,this) {
-
+Ball::Ball():posX(10.0),posY(10.0),velX(1.0),velY(0.5),body(posX, posY, 1, 1, WCYAN, true, false), speed(0.1), angle(45) {
+  apply_velocity();
 }
 
-Ball::Ball(int posX,int posY,float velX,float velY,Color c)
-    :posX((float)posX),posY((float)posY),velX(velX),velY(velY),body(posX, posY, velX, velY, c, this){
-
+// on donne la vitesse et l'angle de la balle au constructeur !
+Ball::Ball(int posX,int posY,float spd,float angl,Color c)
+    :posX((float)posX),posY((float)posY),velX(0),velY(0),body(posX, posY, 1, 1, c, true, false),speed(spd),angle(angl){
+  apply_velocity();
 }
 
 Ball::~Ball(){
-    
+
 }
+
+//Grace a l'angle on récupere la velocité x et y avec cos et sin
+//on multiplie vx et vy par speed pour avoir la vitesse de la balle.
+void Ball::apply_velocity() {
+  float vx, vy = 0;
+  vx = (float)cos(angle*PI/180);
+  vy = (float)sin(angle*PI/180);
+  vx *= speed;
+  vy *= speed;
+  set_vel(vx, vy);
+}
+
 /* Getteurs */
 float Ball::get_posX() const{
     return posX;
@@ -29,6 +46,12 @@ float Ball::get_velX() const{
 }
 float Ball::get_velY() const{
     return velY;
+}
+float Ball::get_speed() const{
+    return speed;
+}
+float Ball::get_angle() const{
+    return angle;
 }
 /* */
 float Ball::get_body_posX(){
@@ -45,6 +68,16 @@ float Ball::get_body_velY(){
 }
 /* */
 /* Setteurs*/
+void Ball::set_angle(float a) { //Définis l'angle de la balle
+    angle = a;
+    apply_velocity();//On applique les changements.
+}
+
+void Ball::set_speed(float s) { //Definis la vitesse de la balle
+    speed = s;
+    apply_velocity();//On applique les changements.
+}
+
 void Ball::set_posX(float x){
     posX = x;
     body.SetPosition(posX,posY);
--- src/ball.h
+++ src/ball.h
@@ -1,5 +1,5 @@
-/* 
-    Declaration de la classe Ball 
+/*
+    Declaration de la classe Ball
     Massili Kezzoul
 */
 #ifndef BALL_H
@@ -10,6 +10,9 @@
 
 class Ball {
 private:
+/* Vitesse et angle*/
+    float speed;
+    float angle;
 /* Position */
     float posX;
     float posY;
@@ -17,7 +20,7 @@ private:
     float velX;
     float velY;
 /* Peut être pour modifié la taille de la balle (actuellement par défault 1 et 1) */
-/* 
+/*
     int width;
     int height;
 */
@@ -25,13 +28,17 @@ private:
 public:
 /* Constructeur */
     Ball();
-    Ball(int posX,int posY,float velX,float velY,Color c = WCYAN);
+    Ball(int posX,int posY,float spd,float angl,Color c = WCYAN);
     ~Ball();
+
+    void apply_velocity();
 /* Getteurs */
     float get_posX() const;
     float get_posY() const;
     float get_velX() const;
     float get_velY() const;
+    float get_speed() const;
+    float get_angle() const;
 
 /* */
     float get_body_posX();
@@ -47,6 +54,8 @@ public:
     void set_velY(float);
     void set_vel(float,float);
     void set_vel(int normal);
+    void set_speed(float);
+    void set_angle(float);
 
 /* Mise à jour de sa position */
     void update();
